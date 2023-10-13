4e3e713335116747804c6846b8bdf895b4c27fb9
Rajeshawal <rajeshawalpro@gmail.com>
2023-10-12 20:14:24 -0500

    fresh_boot

---
+++ team3_backend/src/main/java/team3_backend/team3_backend/Team3BackendApplication.java
@@ -0,0 +1,13 @@
+package team3_backend.team3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3BackendApplication.class, args);
+	}
+
+}
