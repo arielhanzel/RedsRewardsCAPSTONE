4aa9e69383516a49c1eff9c3fcedd926f149b1c6
mattlavergne <c00414984@louisiana.edu>
2023-10-16 16:07:39 -0500

    hamburger icon and progress bar

---
+++ vue_cli_frontend/src/components/HelloWorld.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Earn points to redeem exciting rewards!<br />
+      Learn more about how to earn points
+      <router-link to="/about">here</router-link>.
+    </p>
+
+    <div class="gamification">
+      <div class="points">
+        <span>Points: {{ userPoints }}</span>
+      </div>
+      <div class="progress-bar">
+        <div
+          class="progress"
+          :style="{ width: progressPercentage + '%' }"
+        ></div>
+      </div>
+      <div>
+        <span> {{ pointsToGoal }} points until your next milestone!</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      userPoints: 220,
+      totalPoints: 1000, // Set your desired total points for progress completion
+    };
+  },
+  computed: {
+    progressPercentage() {
+      return (this.userPoints / this.totalPoints) * 100;
+    },
+    pointsToGoal() {
+      return this.totalPoints - this.userPoints;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: rgb(255, 0, 0);
+}
+.gamification {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.points {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.progress-bar {
+  width: 80%;
+  background-color: #eee;
+  margin: 10px auto;
+  border-radius: 5px;
+  height: 20px;
+  overflow: hidden;
+}
+
+.progress {
+  height: 100%;
+  background-color: #4caf50;
+  transition: width 0.3s ease-in-out;
+}
+</style>
---
+++ vue_cli_frontend/src/views/SignUpView.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="signup">
+    <h2>Sign Up</h2>
+    <div class="signup-container">
+      <form @submit.prevent="signup" class="signup-form">
+        <input type="text" placeholder="Username" v-model="username" required />
+        <input type="email" placeholder="Email" v-model="email" required />
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="password"
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+.signup {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+}
+
+.signup h2 {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  color: rgb(255, 0, 0);
+  margin-bottom: -50px;
+}
+
+.signup-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+}
+
+.signup-form {
+  padding: 20px;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  width: 300px;
+  text-align: center;
+}
+
+input[type="email"],
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+button[type="submit"] {
+  font-weight: bold;
+  font-size: larger;
+  height: 30px;
+  width: 100%;
+  color: white;
+  border: none;
+  background-color: rgb(255, 0, 0);
+  margin-top: 10px;
+  border-radius: 15px;
+  cursor: pointer;
+  transition: 0.4s;
+}
+
+button[type="submit"]:hover {
+  color: red;
+  background-color: white;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    signup() {
+      alert("Sign Up functionality to be implemented.");
+    },
+  },
+};
+</script>
